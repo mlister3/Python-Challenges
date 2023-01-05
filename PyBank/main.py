8a04c927d40b0a8f2c1b7681682b5cb789c3767b
mlister3 <matthew.list56@outlook.com>
2023-01-05 18:22:49 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,15 +4,21 @@ import csv
 
 TMonths = 0
 TProfit = 0
+GIncrease = ["", 0]
+GDecrease = ["", 0]
+
 
 Filepath = os.path.join('Resources', 'budget_data.csv')
 
 #print(os.path.abspath(Filepath))
 
-with open(Filepath) as csv_file:
-    csv_reader = csv.reader(csv_file)
-    print(csv_file)
+#with open(Filepath) as csv_file:
+#    csv_reader = csv.reader(csv_file)
+#    print(csv_file)
     
 with open(Filepath, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     print(csvfile)
+
+print("Financial Analysis")
+print("------------------------------------")
