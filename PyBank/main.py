d181c92145732d82d94bf6e6ba7c27b75e1886df
mlister3 <matthew.list56@outlook.com>
2023-01-05 00:04:44 -0500

    Update main.py

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+import os
+
+import csv
+
+TMonths = 0
+TProfit = 0
+
+Filepath = os.path.join('Resources', 'budget_data.csv')
+
+#print(os.path.abspath(Filepath))
+
+with open(Filepath) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    print(csv_file)
+    
+with open(Filepath, encoding='utf') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    print(csvfile)
